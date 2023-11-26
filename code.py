1b75fc56d9959585e65a74fe0d4dd125fcd68713
Logan-Wessel <lgwessel@iastate.edu>
2023-11-25 19:32:04 -0600

    added a bunch of stuff

--- code.py
+++ code.py
@@ -1,20 +1,72 @@
-import time
+import time as t
 from adafruit_clue import clue
-import board
-import busio
+import pwmio, board, busio
 
+
+# Setup stuff
 uart = busio.UART(board.TX, board.RX, baudrate = 9600, timeout = .1)
+speaker = pwmio.PWMOut(board.SPEAKER, variable_frequency = True)
+speaker.frequency = 600 # Hz
+speaker.duty_cycle = 0
+data = clue.simple_text_display(title = "PPP Sensor data", title_scale = 2)
 
+
+# Calibration coefficient from my house
+clue_humidity_calibration = 53 / 42.3
+clue_temperature_calibration = .8526491152
+pm_humidity_calibration = 53 / 49.5
+pm_temperature_calibration = 1.044176707
+
+'''
+Wiring for Clue <-> PM
+0 <-> Up
+1 <-> Down
+3V <-> 3V
+GND <-> GND
+''' 
+
+
+# Temporary bounds for testing
+lower_temperature = 15 # °C
+upper_temperature = 25 # °C
+
+
+# Functions
 def clear():
     print("\n" * 20)
 
-data = clue.simple_text_display(title = "PPP Sensor data", title_scale = 2)
+def show_data():
+    wetness, humidity, temperature = PM_sensors()
+    avg_temperature = ((clue.temperature * clue_temperature_calibration) + temperature) / 2
+    avg_humidity = ((clue.humidity * clue_humidity_calibration) + humidity) / 2
+    light = light_levels()
 
-def clue_sensors():
-    data[0].text = "Pressure:       {:.3f} milibar".format(clue.pressure)
-    data[1].text = "Temperature:    {:.3f} °C".format(clue.temperature)
-    data[2].text = "Humidity:       {:.3f} %".format(clue.humidity)
+    data[0].text = "Soil Wetness:  {:5.0f} %".format(wetness)
+    data[1].text = "Temperature:  {:2.3f} °C".format(avg_temperature)
+    data[2].text = "Humidity:     {:2.3f} %".format(avg_humidity)
+    data[3].text = "Light level:    {:3.2f} L".format(light) # if > 2.5?, theres sunlight
+    
+    if upper_temperature < avg_temperature or avg_temperature < lower_temperature:
+        scream(True)
+        data[5].color = (255, 0, 0)
+        data[5].text = "Temperature of {} fell out of bounds".format(avg_temperature)
+        # print(f"Temperature of {avg_temperature} fell out of bounds")
+    else:
+        scream(False)
+        data[5].text = ""
+
     data.show()
+
+def light_levels():
+    # level = math.floor(clue.color[3] / 100)
+    level = clue.color[3] / 100
+    return level
+
+def scream(state):
+    if state:
+        speaker.duty_cycle = 65000 # int(65535 * .6) # 65535 is 100% volume, speaker bugs out above 65500
+    else:
+        speaker.duty_cycle = 0
 
 def PM_sensors():
     uart.write(b"w")
@@ -22,31 +74,27 @@ def PM_sensors():
     wetness = float(resp[2:-2])
 
     uart.write(b"h")
-    resp = uart.read
-    humidity = float(resp[2:-2])
+    resp = uart.read()
+    humidity = float(resp[2:-2]) * pm_humidity_calibration
 
     uart.write(b"t")
-    resp = uart.read
-    temperature = float(resp[2:-2])
+    resp = uart.read()
+    temperature = float(resp[2:-2]) * pm_temperature_calibration
 
     return wetness, humidity, temperature
+
 
 if __name__ == "__main__":
     clear()
     print("Proficient Python Programmers\nProbe Program P0.01\n")
 
-    uart.write(b"w")
-    resp = uart.read()
-    # wetness = float(resp[2:-2])
-    wetness = resp
-    print(f"Wetness:       {wetness} %")
-
     '''
+    for i in range(10, 251):
+        freq = int(10 * i)
+        print(freq)
+        speaker.frequency = freq
+        time.sleep(.05)
+    #'''
+
     while True:
-        clue_sensors()
-        wetness, humidity, temperature = PM_sensors()
-        print(f"Wetness:       {wetness:.3f} %")
-        print(f"Humidity:       {humidity:.3f} %")
-        print(f"Temperature:    {temperature:.3f} °C")
-    '''
-
+        show_data()
